5ff06ae692cf60f739c65952699bc9b8ef466e0b
Leonardo Marques De Castro Leite <leonardo.leite@prservicos.com.br>
2019-11-22 19:50:47 -0300

    Initial Commit

---
+++ Simulacao3DTests/Simulacao3DTests.swift
@@ -0,0 +1,34 @@
+//
+//  Simulacao3DTests.swift
+//  Simulacao3DTests
+//
+//  Created by Leonardo Marques on 22/11/19.
+//  Copyright © 2019 Leonardo Marques. All rights reserved.
+//
+
+import XCTest
+@testable import Simulacao3D
+
+class Simulacao3DTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
